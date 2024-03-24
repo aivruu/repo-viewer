7761862cf540e060118a0c3d04a91f82fd3355b1
Qekly <benjaalejandro206@gmail.com>
2024-03-24 17:53:06 -0300

    chore: Rename to ReleaseModel

--- src/main/java/me/qeklydev/downloader/release/ReleaseModel.java
+++ src/main/java/me/qeklydev/downloader/release/ReleaseModel.java
@@ -1,8 +1,24 @@
+/*
+ * This file is part of release-downloader - https://github.com/aivruu/release-downloader
+ * Copyright (C) 2020-2024 Aivruu (https://github.com/aivruu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package me.qeklydev.downloader.release;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Arrays;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This record is used to store information about
@@ -11,7 +27,7 @@ import java.util.Arrays;
  * @param version the release version.
  * @since 0.0.1
  */
-public record SerializedReleaseModel(@NotNull String version) {
+public record ReleaseModel(@NotNull String version) {
   /**
    * Returns a string array with every value
    * of the version.
