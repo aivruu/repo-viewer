59e009c16afb72fb5d36fa16dadf1ad54b336b77
Qekly <benjaalejandro206@gmail.com>
2024-03-24 17:56:07 -0300

    chore(http-request-model): Include license header and modify imports positions, provide documentation for the class and methods, and provide full logic for the provideLatestRelease() method

--- src/main/java/me/qeklydev/downloader/http/HTTPModelRequest.java
+++ src/main/java/me/qeklydev/downloader/http/HTTPModelRequest.java
@@ -1,7 +1,21 @@
+/*
+ * This file is part of release-downloader - https://github.com/aivruu/release-downloader
+ * Copyright (C) 2020-2024 Aivruu (https://github.com/aivruu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package me.qeklydev.downloader.http;
-
-import me.qeklydev.downloader.release.SerializedReleaseModel;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,7 +23,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import me.qeklydev.downloader.codec.ReleaseModelDeserializer;
+import me.qeklydev.downloader.release.ReleaseModel;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * This record is used for each HTTP request triggered
+ * to the repository URL given, and provides handling
+ * about the latest release for this repository.
+ *
+ * @param repository the repository url.
+ * @since 0.0.1
+ */
 public record HTTPModelRequest(@NotNull String repository) {
   /**
    * Maximum time for the connection and read HTTP
@@ -19,15 +44,35 @@ public record HTTPModelRequest(@NotNull String repository) {
    */
   private static final byte TIME_OUT = (byte) TimeUnit.MILLISECONDS.toSeconds(60);
 
-  public @NotNull CompletableFuture<@NotNull SerializedReleaseModel[]> provideLatestRelease() {
-    return this.executeGETRequest();
+  /**
+   * Executes the request to the API for receive a json
+   * file with the release information, this file is deserialized
+   * and is returned with the completable-future.
+   *
+   * @return The {@link CompletableFuture} with a
+   *     {@link ReleaseModel} with the latest
+   *     release information.
+   * @since 0.0.1
+   */
+  public @NotNull CompletableFuture<@NotNull ReleaseModel> provideLatestRelease() {
+    return this.executeGETRequest().thenApply(json ->
+        ReleaseModelDeserializer.GSON.fromJson(json, ReleaseModel.class));
   }
 
+  /**
+   * Executes a GET type request to the API with a specific
+   * time-out, the response can be a Json text. Or empty if
+   * an exception is triggered during the operation.
+   *
+   * @return The {@link CompletableFuture} with the final
+   *     request response.
+   * @since 0.0.1
+   */
   public @NotNull CompletableFuture<@NotNull String> executeGETRequest() {
     return CompletableFuture.supplyAsync(() -> {
       try {
         final var urlContentBuilder = new StringBuilder();
-        final var url = new URL(this.repository);
+        final var url = new URL(this.repository + "/releases/latest");
         final var urlConnection = url.openConnection();
         final var httpConnection = (HttpURLConnection) urlConnection;
         httpConnection.setRequestMethod("GET");
