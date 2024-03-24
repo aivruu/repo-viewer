c9b76dde6f771931ff5cbc6a3dc2a8ce13bcf045
Qekly <benjaalejandro206@gmail.com>
2024-03-24 17:54:20 -0300

    chore(io-utils): Change package line and include license header

--- src/main/java/me/qeklydev/downloader/io/IoAsyncUtils.java
+++ src/main/java/me/qeklydev/downloader/io/IoAsyncUtils.java
@@ -1,6 +1,22 @@
+/*
+ * This file is part of release-downloader - https://github.com/aivruu/release-downloader
+ * Copyright (C) 2020-2024 Aivruu (https://github.com/aivruu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package me.qeklydev.downloader.io;
 
-import org.jetbrains.annotations.NotNull;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,6 +24,7 @@ import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This class is used to perform async I/O operations.
