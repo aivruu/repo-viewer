8b5e57cb25193ae4b46296c8f593b2f727b3de89
Qekly <benjaalejandro206@gmail.com>
2024-03-28 22:10:03 -0300

    feat(docs): Implement package-info for license and repository packages

---
+++ src/main/java/me/qeklydev/downloader/license/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides enum to identify the license of type used
+ * on the GitHub repositories.
+ *
+ * @since 0.0.1
+ */
+package me.qeklydev.downloader.license;
---
+++ src/main/java/me/qeklydev/downloader/repository/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides a record model to store the information of
+ * the requested repositories to the GitHub API.
+ *
+ * @since 0.0.1
+ */
+package me.qeklydev.downloader.repository;
