e9483a197f9f7f505fd5efc4d108465031d2a165
Qekly <benjaalejandro206@gmail.com>
2024-05-04 18:11:55 -0400

    chore(io): Add many commentaries and exclude printStackTrace usage in favor of loggers

--- src/main/java/me/qeklydev/downloader/io/IoAsyncUtils.java
+++ src/main/java/me/qeklydev/downloader/io/IoAsyncUtils.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of release-downloader - https://github.com/aivruu/release-downloader
- * Copyright (C) 2020-2024 Aivruu (https://github.com/aivruu)
+ * Copyright (C) 2020-2024 aivruu (https://github.com/aivruu)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,8 +19,7 @@ package me.qeklydev.downloader.io;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
 import java.nio.channels.Channels;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -70,26 +69,23 @@ public final class IoAsyncUtils {
    * @since 0.0.1
    * @see IoAsyncUtils#SINGLE_RETURN_VALUE
    */
-  public static @NotNull CompletableFuture<@NotNull Long> downloadOf(final @NotNull String fileName,
-                                                                     final @NotNull String requestedUrl) {
+  public static @NotNull CompletableFuture<
+      @NotNull Long> downloadOf(final @NotNull String fileName, final @NotNull String requestedUrl) {
     return CompletableFuture.supplyAsync(() -> {
-      URL url;
-      /*
-       * We try to create a new URL object
-       * and try to catch an exception if
-       * provided url isn't valid.
-       */
-      try {
-        url = new URL(requestedUrl);
-      } catch (final MalformedURLException exception) {
-        LoggerUtils.error("URL provided for asset-download is not valid.");
-        return SINGLE_RETURN_VALUE;
-      }
-      try (final var readableByteChannel = Channels.newChannel(url.openStream());
+      // Using the provided URL we create a new URI object with this
+      // same url, this method will throw an exception if given url
+      // syntax is not valid, and will provide a message for error
+      // description.
+      final var uri = URI.create(requestedUrl);
+      // We convert the URI instance into a URL object, and then we open
+      // the stream for channel data reading and write bytes to this channel
+      // at the given position.
+      try (final var readableByteChannel = Channels.newChannel(uri.toURL().openStream());
            final var fileOutputStream = new FileOutputStream(fileName)) {
-        return fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        return fileOutputStream.getChannel().transferFrom(
+            readableByteChannel, /* The initial position for bytes transfer. */ 0, Long.MAX_VALUE);
       } catch (final IOException exception) {
-        exception.printStackTrace();
+        LoggerUtils.error("URL content download has suffered an exception during the process.");
         return SINGLE_RETURN_VALUE;
       }
     }, IO_POOL_THREAD);
