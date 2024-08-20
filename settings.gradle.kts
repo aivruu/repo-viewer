857ea587e64be3a46464b3237bd03c74c7159b26
aivruu <benjaalejandro206@gmail.com>
2024-08-19 21:43:56 -0400

    chore(projects): Include sub-projects on project's settings file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
-rootProject.name = "release-downloader"
+@file:Suppress("UnstableApiUsage")
+
+rootProject.name = "repo-viewer"
+
+sequenceOf("api", "implementation").forEach {
+  val kerbalProject = ":${rootProject.name}-$it"
+  include(kerbalProject)
+  project(kerbalProject).projectDir = file(it)
+}
