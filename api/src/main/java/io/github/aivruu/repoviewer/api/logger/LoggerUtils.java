ca0b402031ba474c43dd02f89c4ad20ae72593df
aivruu <benjaalejandro206@gmail.com>
2024-08-19 22:01:32 -0400

    chore(LoggerUtils): Suffered package relocation

--- api/src/main/java/io/github/aivruu/repoviewer/api/logger/LoggerUtils.java
+++ api/src/main/java/io/github/aivruu/repoviewer/api/logger/LoggerUtils.java
@@ -1,21 +1,20 @@
-/*
- * This file is part of release-downloader - https://github.com/aivruu/release-downloader
- * Copyright (C) 2020-2024 aivruu (https://github.com/aivruu)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-package me.qeklydev.downloader.logger;
+//
+// Copyright (C) 2024 Aivruu - repo-viewer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+package io.github.aivruu.repoviewer.api.logger;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
