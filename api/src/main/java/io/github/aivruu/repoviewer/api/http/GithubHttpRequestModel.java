7c62b4c75302ce6b55d75eb2260fffe0be261a74
aivruu <benjaalejandro206@gmail.com>
2024-08-24 21:56:48 -0400

    chore(http-model): Include ResponseStatusProvider usage and implement static-function for responses' disponibility and their status-codes verification

--- api/src/main/java/io/github/aivruu/repoviewer/api/http/GithubHttpRequestModel.java
+++ api/src/main/java/io/github/aivruu/repoviewer/api/http/GithubHttpRequestModel.java
@@ -16,10 +16,13 @@
 //
 package io.github.aivruu.repoviewer.api.http;
 
+roivimport io.github.aivruu.repoviewer.api.codec.CodecProvider;
 import io.github.aivruu.repoviewer.api.http.request.RequestableModel;
+import io.github.aivruu.repoviewer.api.http.status.ResponseStatusProvider;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -41,10 +44,19 @@ public interface GithubHttpRequestModel<Model extends RequestableModel> {
    *
    * @param httpClient the {@link HttpClient} to use for this request.
    * @param timeout the time-out for the request.
-   * @return The {@link CompletableFuture} with a {@code nullable} value.
+   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, this provider will have
+   *     a possible {@code null} model.
+   *     <p>
+   *     - {@link ResponseStatusProvider#requestValidResponse(RequestableModel)} if the response is valid.
+   *     <p>
+   *     - {@link ResponseStatusProvider#requestBadResponse()} if the response's status-code is '400' (bad).
+   *     <p>
+   *     - {@link ResponseStatusProvider#requestUnknownResponse()} if the response's status-code is '404' (not found).
+   *     <p>
+   *     - {@link ResponseStatusProvider#requestInvalidResponse()} if the response wasn't provided.
    * @since 2.3.4
    */
-  CompletableFuture<@Nullable Model> requestUsing(final HttpClient httpClient, final int timeout);
+  CompletableFuture<ResponseStatusProvider<@Nullable Model>> requestUsing(final HttpClient httpClient, final int timeout);
 
   /**
    * Executes the {@code GET} request-type using the specified {@link HttpClient}, then, if the
@@ -56,25 +68,36 @@ public interface GithubHttpRequestModel<Model extends RequestableModel> {
    * @param timeout the time-out for the request.
    * @param consumer the logic to execute with the given model, if the request produced a
    *                 response ({@code json-body}).
-   * @return The {@link CompletableFuture} with a {@code nullable} model.
+   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, this provider will have
+   *    a possible {@code null} model.
+   *    <p>
+   *    - {@link ResponseStatusProvider#requestValidResponse(RequestableModel)} if the response is valid.
+   *    <p>
+   *    - {@link ResponseStatusProvider#requestBadResponse()} if the response's status-code is '400' (bad).
+   *    <p>
+   *    - {@link ResponseStatusProvider#requestUnknownResponse()} if the response's status-code is '404' (not found).
+   *    <p>
+   *    - {@link ResponseStatusProvider#requestInvalidResponse()} if the response wasn't provided.
    * @since 2.3.4
    */
-  CompletableFuture<@Nullable Model> requestUsingThen(final HttpClient httpClient, final int timeout, Consumer<Model> consumer);
+  CompletableFuture<ResponseStatusProvider<@Nullable Model>> requestUsingThen(final HttpClient httpClient, final int timeout,
+                                                                              final Consumer<Model> consumer);
 
   /**
    * Executes the {@code GET} request-type using a default new {@link HttpClient}, once the
    * request has ended, the early created {@link HttpClient} is closed.
    *
    * @param timeout the time-out for this request.
-   * @return The {@link CompletableFuture} with a {@code nullable} value.
+   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, this provider will have
+   *     a possible {@code null} model.
    * @see #requestUsing(HttpClient, int)
    * @since 2.3.4
    */
-  default CompletableFuture<@Nullable Model> request(final int timeout) {
+  default CompletableFuture<ResponseStatusProvider<@Nullable Model>> request(final int timeout) {
     final var httpClient = HttpClient.newHttpClient();
-    final var requestModelResponse = this.requestUsing(httpClient, timeout);
+    final var requestResponseStatusProvider = this.requestUsing(httpClient, timeout);
     httpClient.close(); // Close http-client after the request.
-    return requestModelResponse;
+    return requestResponseStatusProvider;
   }
 
   /**
@@ -85,14 +108,50 @@ public interface GithubHttpRequestModel<Model extends RequestableModel> {
    * @param timeout the time-out for this request.
    * @param consumer the logic to execute with the given model, if the request produced a
    *                 response ({@code json-body}).
-   * @return The {@link CompletableFuture} with a {@code nullable} model.
+   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, this provider will have
+   *     a possible {@code null} model.
    * @see #requestUsingThen(HttpClient, int, Consumer)
    * @since 2.3.4
    */
-  default CompletableFuture<@Nullable Model> requestThen(final int timeout, Consumer<Model> consumer) {
+  default CompletableFuture<ResponseStatusProvider<@Nullable Model>> requestThen(final int timeout, final Consumer<Model> consumer) {
     final var httpClient = HttpClient.newHttpClient();
-    final var requestModelResponse = this.requestUsingThen(httpClient, timeout, consumer);
+    final var requestResponseStatusProvider = this.requestUsingThen(httpClient, timeout, consumer);
     httpClient.close(); // Close http-client after the request.
-    return requestModelResponse;
+    return requestResponseStatusProvider;
+  }
+
+  /**
+   * Returns a {@link ResponseStatusProvider} based on the response-disponibility, and their status-code.
+   *
+   * @param response the response provided by the http-request.
+   * @param consumer the consumer for the deserialized-model, {@code null} on functions that doesn't
+   *                 execute additional logic with deserialized-models.
+   * @param codecProvider a {@link CodecProvider} implementation.
+   * @param modelType the model-type to deserialize if response and status-code are valid.
+   * @param <Model> an object which implements the {@link RequestableModel} interface.
+   * @return The {@link ResponseStatusProvider} with the deserialized-model, or {@code null}.
+   * @since 2.3.4
+   */
+  static <Model extends RequestableModel> ResponseStatusProvider<@Nullable Model> verifyAndProvideResponse(
+    final @Nullable HttpResponse<String> response, final @Nullable Consumer<Model> consumer, final CodecProvider codecProvider,
+    final Class<Model> modelType
+  ) {
+    if (response == null) {
+      return ResponseStatusProvider.requestInvalidResponse();
+    }
+    final var statusCode = response.statusCode();
+    if (statusCode == 404) {
+      return ResponseStatusProvider.requestUnknownResponse();
+    } else if (statusCode == 400) {
+      return ResponseStatusProvider.requestBadResponse();
+    }
+    final var model = codecProvider.from(modelType, response.body());
+    if (model == null) {
+      return ResponseStatusProvider.requestInvalidResponse();
+    }
+    if (consumer != null) {
+      consumer.accept(model);
+    }
+    return ResponseStatusProvider.requestValidResponse(model);
   }
 }
