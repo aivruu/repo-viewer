4da814747445aaa9acd82d66fc2d3a568c3c637f
aivruu <benjaalejandro206@gmail.com>
2024-08-26 12:28:07 -0400

    chore(http-model): Modify documentation for some functions, and move two methods to RequestUtils class

--- api/src/main/java/io/github/aivruu/repoviewer/api/http/GithubHttpRequestModel.java
+++ api/src/main/java/io/github/aivruu/repoviewer/api/http/GithubHttpRequestModel.java
@@ -24,36 +24,23 @@ import org.jetbrains.annotations.Nullable;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 /**
  * This base-model for another request-implementations such as for releases or repositories.
  *
  * @param <Model> The model-type to use for this request.
- * @since 1.0.0
+ * @since 3.3.4
  */
 public interface GithubHttpRequestModel<Model extends RequestableModel> {
-  /** Four fixed-thread-pool used for http-requests operations. */
-  ExecutorService EXECUTOR = Executors.newFixedThreadPool(4, r -> new Thread(r, "HttpRequest-Thread"));
-
   /**
    * Executes the {@code GET} request-type using the specified {@link HttpClient}, and return
    * the serialized-model if a {@code json-body} was provided, otherwise result will be a {@code null}.
    *
    * @param httpClient the {@link HttpClient} to use for this request.
    * @param timeout the time-out for the request.
-   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, this provider will have
-   *     a possible {@code null} model.
-   *     <p>
-   *     - {@link ResponseStatusProvider#requestValidResponse(RequestableModel)} if the response is valid.
-   *     <p>
-   *     - {@link ResponseStatusProvider#requestBadResponse()} if the response's status-code is '400' (bad).
-   *     <p>
-   *     - {@link ResponseStatusProvider#requestUnknownResponse()} if the response's status-code is '404' (not found).
-   *     <p>
-   *     - {@link ResponseStatusProvider#requestInvalidResponse()} if the response wasn't provided.
+   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, check possible responses
+   *     at {@link RequestUtils#verifyAndProvideResponse(HttpResponse, Consumer, CodecProvider, Class)}.
    * @since 2.3.4
    */
   CompletableFuture<ResponseStatusProvider<@Nullable Model>> requestUsing(final HttpClient httpClient, final int timeout);
@@ -68,16 +55,8 @@ public interface GithubHttpRequestModel<Model extends RequestableModel> {
    * @param timeout the time-out for the request.
    * @param consumer the logic to execute with the given model, if the request produced a
    *                 response ({@code json-body}).
-   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, this provider will have
-   *    a possible {@code null} model.
-   *    <p>
-   *    - {@link ResponseStatusProvider#requestValidResponse(RequestableModel)} if the response is valid.
-   *    <p>
-   *    - {@link ResponseStatusProvider#requestBadResponse()} if the response's status-code is '400' (bad).
-   *    <p>
-   *    - {@link ResponseStatusProvider#requestUnknownResponse()} if the response's status-code is '404' (not found).
-   *    <p>
-   *    - {@link ResponseStatusProvider#requestInvalidResponse()} if the response wasn't provided.
+   * @return The {@link CompletableFuture} with a {@link ResponseStatusProvider}, check possible responses
+   *     at {@link RequestUtils#verifyAndProvideResponse(HttpResponse, Consumer, CodecProvider, Class)}.
    * @since 2.3.4
    */
   CompletableFuture<ResponseStatusProvider<@Nullable Model>> requestUsingThen(final HttpClient httpClient, final int timeout,
@@ -118,40 +97,5 @@ public interface GithubHttpRequestModel<Model extends RequestableModel> {
     final var requestResponseStatusProvider = this.requestUsingThen(httpClient, timeout, consumer);
     httpClient.close(); // Close http-client after the request.
     return requestResponseStatusProvider;
-  }
-
-  /**
-   * Returns a {@link ResponseStatusProvider} based on the response-disponibility, and their status-code.
-   *
-   * @param response the response provided by the http-request.
-   * @param consumer the consumer for the deserialized-model, {@code null} on functions that doesn't
-   *                 execute additional logic with deserialized-models.
-   * @param codecProvider a {@link CodecProvider} implementation.
-   * @param modelType the model-type to deserialize if response and status-code are valid.
-   * @param <Model> an object which implements the {@link RequestableModel} interface.
-   * @return The {@link ResponseStatusProvider} with the deserialized-model, or {@code null}.
-   * @since 2.3.4
-   */
-  static <Model extends RequestableModel> ResponseStatusProvider<@Nullable Model> verifyAndProvideResponse(
-    final @Nullable HttpResponse<String> response, final @Nullable Consumer<Model> consumer, final CodecProvider codecProvider,
-    final Class<Model> modelType
-  ) {
-    if (response == null) {
-      return ResponseStatusProvider.requestInvalidResponse();
-    }
-    final var statusCode = response.statusCode();
-    if (statusCode == 404) {
-      return ResponseStatusProvider.requestUnknownResponse();
-    } else if (statusCode == 400) {
-      return ResponseStatusProvider.requestBadResponse();
-    }
-    final var model = codecProvider.from(modelType, response.body());
-    if (model == null) {
-      return ResponseStatusProvider.requestInvalidResponse();
-    }
-    if (consumer != null) {
-      consumer.accept(model);
-    }
-    return ResponseStatusProvider.requestValidResponse(model);
   }
 }
