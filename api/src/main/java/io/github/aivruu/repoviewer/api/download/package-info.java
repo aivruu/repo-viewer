e825d2d3ddcc38f7396221d2d8ffa079b0f882aa
aivruu <benjaalejandro206@gmail.com>
2025-03-22 19:12:28 -0300

    refactor!: remove old structure and code

--- api/src/main/java/io/github/aivruu/repoviewer/api/download/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides utilities and status-code providers for I/O operations.
- *
- * @since 0.0.1
- */
-package io.github.aivruu.repoviewer.api.download;
--- implementation/src/main/java/io.github.aivruu.repoviewer/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides URL-Builder provider for repository/release models, http-implementations for
- * this models, and codec implementations for deserialization of these models.
- *
- * @since 2.3.4
- */
-package io.github.aivruu.repoviewer;
